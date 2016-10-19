4750872eda33a4ad2bf6acc3ffb7ff8b74b2291b
olpotkin <olpotkin@gmail.com>
2016-10-19 09:34:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+######################
+# Oleg Potkin        #
+# olpotkin@gmail.com #
+######################
+
+# TODO: 1. Start tcpdump
+
+# TODO: 2. Stop tcpdump
+
+# TODO: 3. Start tshark (convert .pcap to .json)
+
+# TODO: 4. Analytics
