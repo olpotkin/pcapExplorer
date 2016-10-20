a2b8277358dc87b207458be409524d2443b2315a
olpotkin <olpotkin@gmail.com>
2016-10-20 12:33:57 -0700

    Open json and filter IP packs

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ import json
 
 # Start tcpdump
 # Command >> tcpdump -I -i en1 -w ~/Desktop/output.pcap
+
 p = subprocess.Popen(['tcpdump', "-I", "-i", "en1",
                       '-w', 'cap.pcap'], stdout=subprocess.PIPE, shell=False)
-time.sleep(60)      # Capturing traffic for 30 seconds
+time.sleep(60)      # Capturing traffic for N seconds
 p.terminate()       # Stop tcpdump
 time.sleep(1)
 
 # Check if the subprocess has terminated
 # If not - force kill
+
 try:
     os.kill(p.pid, 0)
     p.kill()
@@ -28,6 +30,7 @@ except OSError, e:
 
 # Start tshark (convert .pcap to .json)
 # Command >> tshark -r cap.pcap -Tjson > cap.json
+
 tsharkCommand = ['tshark','-r', 'cap.pcap', '-Tjson',
                  '-e', 'frame.number',
                  '-e', 'wlan_radio.channel',
@@ -44,12 +47,27 @@ time.sleep(1)
 
 
 # Analytics
-# 1. Open json
-# 2. Filter only packages with QoS marks:
-#   - package ID
-#   - Src IP
-#   - Dst IP
-#   - QoS value
-#   - DSCP value
+# Open json
+with open('cap.json') as json_data:
+    dict = json.load(json_data)
+    # Filter IP packs with QoS:
+    #   - Src IP
+    #   - Dst IP
+    #   - QoS value
+    #   - package ID
+    iterator = 1
+    for item in dict:
+        try:
+            print item["_source"]["layers"]["ip.src"]
+            print item["_source"]["layers"]["ip.dst"]
+            print item["_source"]["layers"]["wlan.qos.priority"]
+            print item["_source"]["layers"]["frame.number"]
+            print iterator
+            print "===============\n"
+            iterator += 1
+        except:
+            continue
+
+
 # 3. Filter by IPs (srs->dst, dst->src)
 # 4. Show result list
